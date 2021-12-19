063973be9f844827ad265b53fd89c7c5eb368f33
Cahyadi <cahyadisn6@gmail.com>
2021-12-20 06:08:28 +0700

    Menambah logo.png dan membuat frame window

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import javax.swing.*;
+
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        // Make JFrame window
+        JFrame frame = new JFrame();
 
+        ImageIcon icon = new ImageIcon("D:\\User\\Cahyadi\\Documents\\Project\\Java Project\\AVANLON\\res\\screen\\logo.png");
+        frame.setIconImage(icon.getImage());
+
+        frame.setTitle("AVANLON");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
