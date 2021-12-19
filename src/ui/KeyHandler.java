bcfe5ccfd1136077d8216819f432b1ea235baa7c
Cahyadi <cahyadisn6@gmail.com>
2021-12-20 06:46:34 +0700

    Menambah class Keyhandler

---
+++ src/ui/KeyHandler.java
@@ -0,0 +1,48 @@
+package ui;
+
+import ui.GamePanel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener
+{
+    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed;
+    GamePanel gp;
+
+    public KeyHandler(GamePanel gp)
+    {
+        this.gp = gp;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        // Key Handling at play state
+        if(gp.gameState == gp.playState)
+        {
+            switch (e.getKeyCode())
+            {
+                case KeyEvent.VK_W -> upPressed = true;
+                case KeyEvent.VK_S -> downPressed = true;
+                case KeyEvent.VK_A -> leftPressed = true;
+                case KeyEvent.VK_D -> rightPressed = true;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+        switch (e.getKeyCode())
+        {
+            case KeyEvent.VK_W -> upPressed = false;
+            case KeyEvent.VK_S -> downPressed = false;
+            case KeyEvent.VK_A -> leftPressed = false;
+            case KeyEvent.VK_D -> rightPressed = false;
+        }
+    }
+}
